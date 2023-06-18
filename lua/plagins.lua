56338babe95caa8c97be0cce634c9eede019db20
danilbushkov <danil1357997@gmail.com>
2023-06-18 19:51:33 +0300

    move plagins config from plagins.lua to plagins folder

--- lua/plagins.lua
+++ lua/plagins.lua
@@ -11,6 +11,9 @@ return require('packer').startup(function(use)
       "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
     },
+    config = function()
+      require("plagins/neotree")
+    end
   }
 
   use {
@@ -52,32 +55,7 @@ return require('packer').startup(function(use)
   use {
     "hrsh7th/nvim-cmp",
     config = function()
-      local cmp = require 'cmp'
-      cmp.setup({
-        completion = {
-          autocomplete = false
-        },
-        snippet = {
-          expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-          end,
-        },
-        window = {
-          -- completion = cmp.config.window.bordered(),
-          -- documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true })
-        }),
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-        })
-      })
+      require("plagins/cmp")
     end
   }
 end)
