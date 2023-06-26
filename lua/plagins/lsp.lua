f134d8a95cae75ede87cb1c8c195749293315a72
danilbushkov <danil1357997@gmail.com>
2023-06-26 19:18:47 +0300

    add format code after save file for eslint

--- lua/plagins/lsp.lua
+++ lua/plagins/lsp.lua
@@ -1,7 +1,15 @@
 local lspconfig = require("lspconfig")
 
 
-lspconfig.eslint.setup({})
+lspconfig.eslint.setup({
+  on_attach = function(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "EslintFixAll",
+    })
+  end,
+})
+
 lspconfig.lua_ls.setup({
   settings = {
     Lua = {
