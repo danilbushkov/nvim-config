1f1643375b9329de0e1f1227b6ab4ecda9f03677
danilbushkov <danil1357997@gmail.com>
2023-11-18 10:30:36 +0300

    add mappings for neotree

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,5 +2,7 @@ function nmap(key, command)
   vim.keymap.set("n", key, command)
 end
 
-nmap("<C-p>", "<cmd>Neotree focus toggle<cr>")
+nmap("<C-f>", "<cmd>Neotree toggle<cr>")
 nmap("<C-a>", "<cmd>Neotree focus<cr>")
+nmap("<C-b>", "<cmd>Neotree toggle focus buffers right<cr>")
+nmap("<C-s>", "<cmd>Neotree float git_status<cr>")
