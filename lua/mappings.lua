652bdc0ab87a6f90c531832107fa137c690667b9
danilbushkov <danil1357997@gmail.com>
2024-01-09 13:35:39 +0300

    add reveal to neotree mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,8 +1,8 @@
-function nmap(key, command)
+local function nmap(key, command)
   vim.keymap.set("n", key, command)
 end
 
-nmap("<C-f>", "<cmd>Neotree toggle<cr>")
-nmap("<C-a>", "<cmd>Neotree focus<cr>")
-nmap("<C-b>", "<cmd>Neotree toggle focus buffers right<cr>")
+nmap("<C-f>", "<cmd>Neotree toggle reveal<cr>")
+nmap("<C-a>", "<cmd>Neotree reveal<cr>")
+nmap("<C-b>", "<cmd>Neotree toggle reveal buffers right<cr>")
 nmap("<C-s>", "<cmd>Neotree float git_status<cr>")
