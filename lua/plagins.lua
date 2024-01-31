5f51cc7617eb5116719aa979696c654f63a0a191
danilbushkov <danil1357997@gmail.com>
2024-01-31 17:24:24 +0300

    update telescope

--- lua/plagins.lua
+++ lua/plagins.lua
@@ -77,7 +77,7 @@ return require("packer").startup(function(use)
   }
 
   use {
-    "nvim-telescope/telescope.nvim", tag = "0.1.1",
+    "nvim-telescope/telescope.nvim", tag = "0.1.5",
     requires = { "nvim-lua/plenary.nvim" },
     config = function()
       require("plagins/telescope")
