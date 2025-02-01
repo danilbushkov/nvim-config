adba7092092766f05ba403e05d8d4f373ce95f86
danilbushkov <danil1357997@gmail.com>
2025-02-01 17:41:47 +0300

    add lsp for Scala

--- lua/plugins/lsp/config.lua
+++ lua/plugins/lsp/config.lua
@@ -75,8 +75,6 @@ return function()
 
   lspconfig.lua_ls.setup({})
 
-  lspconfig.metals.setup({})
-
 
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   capabilities.textDocument.completion.completionItem.snippetSupport = true
