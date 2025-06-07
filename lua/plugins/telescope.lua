d91db4aadc9c5733a5de31b7bc0ee13eb01510d9
danilbushkov <danil1357997@gmail.com>
2025-06-07 21:41:28 +0300

    add keymaps for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,12 +2,34 @@ return {
   "nvim-telescope/telescope.nvim",
   tag = "0.1.8",
   dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-    vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-    vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+  keys = {
+    { "<leader>ff", "<cmd>Telescope find_files<cr>" },
+    { "<leader>fg", "<cmd>Telescope live_grep<cr>" },
+    { "<leader>fb", "<cmd>Telescope buffers<cr>" },
+    { "<leader>fh", "<cmd>Telescope help_tags<cr>" },
+    { "<leader>fr", "<cmd>Telescope resume<cr>" },
+    { "<leader>fh", "<cmd>Telescope pickers<cr>" }
+  },
+  opts = function()
+    local layout = require("telescope.actions.layout")
+    local actions = require("telescope.actions")
+    return {
+      defaults = {
+        cache_picker = {
+          num_pickers = 50,
+        },
+        mappings = {
+          i = {
+            ["<C-s>"] = layout.toggle_preview,
+            ["<M-n>"] = actions.cycle_history_next,
+            ["<M-p>"] = actions.cycle_history_prev
+          },
+          n = {
+            ["<C-s>"] = layout.toggle_preview
+          }
+        }
+      }
+    }
   end
 
 }
