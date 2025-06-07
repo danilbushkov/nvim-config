56fe6521916f02d285365a0362003710a4834a22
danilbushkov <danil1357997@gmail.com>
2025-06-07 22:00:41 +0300

    add neogit

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,14 @@
+return {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+
+    "nvim-telescope/telescope.nvim",
+    "ibhagwan/fzf-lua",
+    "echasnovski/mini.pick",
+    "folke/snacks.nvim",
+
+
+  },
+}
