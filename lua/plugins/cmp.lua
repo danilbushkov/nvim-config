9d08a4c0f056685367d0f6e7193cc32e682317ec
danilbushkov <danil1357997@gmail.com>
2025-09-13 19:10:03 +0300

    change autocompletion on Tab key

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -25,7 +25,7 @@ return {
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<Tab>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
         ["<CR>"] = cmp.mapping.confirm({ select = true })
       }),
