0c922774e8c0cf2f14aaefbb61f2ba4969e58502
danilbushkov <danil1357997@gmail.com>
2025-10-11 23:14:38 +0300

    move treesitter

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-
 local opt = vim.opt
+
+opt.tabstop = 4
+opt.shiftwidth = 4
 
 opt.listchars = { tab = ">~", space = "·" }
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,32 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "c",
+      "cpp",
+      "lua",
+      "rust",
+      "go",
+      "css",
+      "html",
+      "javascript",
+      "json",
+      "make",
+      "sql",
+      "toml",
+      "typescript",
+      "yaml",
+      "bash",
+      "dockerfile",
+      "tsx",
+      "php",
+      "java",
+      "scala",
+      "markdown",
+    },
+    highlight = {
+      enable = true,
+    },
+    indent = { enable = true },
+  },
+}
