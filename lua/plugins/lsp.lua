e67ff73dd66f422ae18ed5b768a6870a5a8e3f9d
danilbushkov <danil1357997@gmail.com>
2025-11-29 21:06:32 +0300

    add rust_analyzer and docker_compose_server

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,11 +4,13 @@ return {
     servers = {
       nil_ls = {},
       gopls = {},
+      rust_analyzer = {},
 
       intelephense = {},
       phpactor = {},
 
       dockerls = {},
+      docker_language_server = {},
       yamlls = {},
     },
   },
